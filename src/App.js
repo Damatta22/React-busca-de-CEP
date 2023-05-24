74daa9ac6014a90caeba526d82db4f0222c159a9
Damatta22 <flaviodamataflv@gmail.com>
2023-05-24 20:22:38 -0300

    atualized

--- src/App.js
+++ src/App.js
@@ -3,16 +3,34 @@ import React from 'react';
 
 import { FiSearch } from 'react-icons/fi';
 import './styles.css';
-
+import api from './services/api'
 
 
 function App() {
 
   const [input, setInput] = useState('')
+  const [cep, setCep] = useState({});
+
+  async function handleSearch() {
+   
+
+    if (input === "") {
+      alert("Preencha algum cep!")
+      return;
+    }
+
+    try {
+      const response = await api.get(`${input}/json`);
+      setCep(response.data)
+      setInput("");
+
+    } catch {
+
+      alert("Ops erro ao buscar");
+      setInput("")
+    }
 
 
-  function handleSearch(){
-    alert('Cliquei')
   }
 
 
@@ -20,7 +38,7 @@ function App() {
   return (
     <div className="container">
 
-      <h1 className="title">Buscador CEP</h1>
+      <h1 className="title">Find your cep Adress</h1>
 
 
 
@@ -31,21 +49,26 @@ function App() {
           placeholder="Digite o CEP"
           value={input}
           onChange={(e) => setInput(e.target.value)} />
+
         <button className="buttonSearch"
-          onclick={handleSearch}>
+          onClick={handleSearch}>
           <FiSearch size={25} color="#FFF" />
         </button>
 
       </div>
 
-      <main className="main">
-        <h2>CEP: 35701168</h2>
-        <span>Rua Santa Rita do Sapucai</span>
-        <span>Complemento: Apartamento C701</span>
-        <span>Bairro: Valle das Palmeiras</span>
-        <span>Cidade: Sete Lagoas - MG</span>
 
-      </main>
+      {Object.keys(cep).length > 1 && (
+        <main className="main">
+          <h2>CEP: {cep.cep}</h2>
+          <span>{cep.logradouro}</span>
+          <span>{cep.complemento}</span>
+          <span>{cep.bairro}</span>
+          <span>{cep.localidade} - {cep.uf}</span>
+
+        </main>
+        )}
+
     </div>
   );
 }
